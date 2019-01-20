d6174f8d8ed0d86c98d5a6b6b8af94b862d7caed
Phonekham <30440409+Phonekham@users.noreply.github.com>
2019-01-20 16:08:04 +0700

    object literal

--- app.js
+++ app.js
@@ -86,49 +86,82 @@
 
 // document.body.innerHTML = html;
 
-//array
-const numbers = [34,44,66,3,54];
-const numbers2 = new Array(23,89,9,34,23);
-const fruit = ['Apple','Banna','orange'];
-const mixed = [22,"hi",true,undefined,null,{a:1,b:2},new Date()];
-let val;
-//get array lenght
-val = numbers.length;
-//check if array
-val = Array.isArray(numbers);
-val = numbers[3];
-//insert into array
-numbers2[2] = 100
-//mutateing array
-numbers2.push(22); //add to last
-numbers2.unshift(120); //add to first
-//take off from end
-numbers2.pop();
-//take off from front
-numbers2.shift();
-//splice
-numbers2.slice(1,3);
+// //array
+// const numbers = [34,44,66,3,54];
+// const numbers2 = new Array(23,89,9,34,23);
+// const fruit = ['Apple','Banna','orange'];
+// const mixed = [22,"hi",true,undefined,null,{a:1,b:2},new Date()];
+// let val;
+// //get array lenght
+// val = numbers.length;
+// //check if array
+// val = Array.isArray(numbers);
+// val = numbers[3];
+// //insert into array
+// numbers2[2] = 100
+// //mutateing array
+// numbers2.push(22); //add to last
+// numbers2.unshift(120); //add to first
+// //take off from end
+// numbers2.pop();
+// //take off from front
+// numbers2.shift();
+// //splice
+// numbers2.slice(1,3);
 
-//concatenate array
-val = numbers.concat(numbers2);
-//sorting array
-val = fruit.sort();
-val = numbers.sort();
+// //concatenate array
+// val = numbers.concat(numbers2);
+// //sorting array
+// val = fruit.sort();
+// val = numbers.sort();
 
-//use the compare function
-val = numbers.sort(function(x,y){
-return x-y;
-});
-//reverse sort
-val = numbers.sort(function (x, y) {
-  return y-x;
-});
+// //use the compare function
+// val = numbers.sort(function(x,y){
+// return x-y;
+// });
+// //reverse sort
+// val = numbers.sort(function (x, y) {
+//   return y-x;
+// });
 
-//find
-function under50(num){
-  return num < 50;
+// //find
+// function under50(num){
+//   return num < 50;
+// }
+// val = numbers.find(under50);
+
+// console.log(numbers2);
+// console.log(val);
+
+const person = {
+  firstname:'Phonekham',
+  lastname:'Keomany',
+  age:23,
+  email:'phone@gmail.com',
+  hobbies:['music','coding','sports'],
+  address:{
+    city:'VT',
+    village:'khamhoung'
+  },
+  GetBirthYear:function(){
+    return 2018 - this.age;
+  }
 }
-val = numbers.find(under50);
-
-console.log(numbers2);
+let val;
+val = person;
+//get specific value
+val = person.firstname;
+val = person['lastname'];
+val = person.age;
+val = person.hobbies[1];
+val = person.address.city;
+val = person.address['village'];
+val = person.GetBirthYear();
 console.log(val);
+const people = [
+  {name:'Phone',age:23},
+  {name:'seng',age:22}
+];
+for(let i = 0; i < people.length;i++){
+  console.log(people[i].name);
+}
