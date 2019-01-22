85c3dd51cfbae96c96621bd75abb668e30e8cbcd
Phonekham <30440409+Phonekham@users.noreply.github.com>
2019-01-22 20:12:32 +0700

    DOM:Loan calculator part 3 loader

--- Loan Calculator/app.js
+++ Loan Calculator/app.js
@@ -1,8 +1,19 @@
 //listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+  
+  //hide results
+  document.getElementById('results').style.display = 'none';
+  //show loader
+  document.getElementById('loading').style.display = 'block';  
+
+  setTimeout(calculateResults,2000);
+
+
+  e.preventDefault();
+});
 
 //calculate results
-function calculateResults(e){
+function calculateResults(){
  //UI vars
  const amount = document.getElementById('amount');
  const interest = document.getElementById('interest');
@@ -23,14 +34,22 @@ function calculateResults(e){
    monthlyPayment.value = monthly.toFixed(2);
    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
    totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    //show results
+    document.getElementById('results').style.display = 'block';
+     //hide loader
+     document.getElementById('loading').style.display = 'none';
  }else{
   showError('Please check your numbers');
  }
- e.preventDefault();
+ 
 }
 
 //show error 
 function showError(error){
+  //show results
+  document.getElementById('results').style.display = 'none';
+  //hide loader
+  document.getElementById('loading').style.display = 'none';
   //Create a div
   const errorDiv  = document.createElement('div');
 
