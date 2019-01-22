acf5ea8119eca28c2a1f6e5131b9512e014798ba
Phonekham <30440409+Phonekham@users.noreply.github.com>
2019-01-22 23:11:48 +0700

    OOP prototype

--- oop.js
+++ oop.js
@@ -1,13 +1,18 @@
-//person instructor
-function Person(name,dob){
-  this.name = name;
-  this.birthday = new Date(dob);
-  this.calculateAge = function(){
-    const diff = Date.now() - this.birthday.getTime();
-    const ageDate = new Date(diff);
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
-  }
-}
-const Phone = new Person('Phone','07/03/1981');
-const jo = new Person('Jo');
-console.log(Phone.calculateAge());
+// //person instructor
+// function Person(name,dob){
+//   this.name = name;
+//   this.birthday = new Date(dob);
+//   this.calculateAge = function(){
+//     const diff = Date.now() - this.birthday.getTime();
+//     const ageDate = new Date(diff);
+//     return Math.abs(ageDate.getUTCFullYear() - 1970);
+//   }
+// }
+// const Phone = new Person('Phone','07/03/1981');
+// const jo = new Person('Jo');
+// console.log(Phone.calculateAge());
+
+// string 
+const name1 = 'Ju';
+const name2 = new String('Ju');
+console.log(name2);
