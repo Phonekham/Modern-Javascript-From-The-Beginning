7cc3fbc53770495591a319e2dda3c3f67817812d
Phonekham <30440409+Phonekham@users.noreply.github.com>
2019-01-26 19:51:36 +0700

    075 Github Finder Part 5 - Fetch  Display Repos

--- API Projects/Github Finder/github.js
+++ API Projects/Github Finder/github.js
@@ -2,14 +2,20 @@ class Github{
   constructor(){
     this.client_id = '1b7f186e11eaf7380579';
     this.client_secret = '113f765e67ac247c1a3f578cccb235bbb3771f4b';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user){
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
