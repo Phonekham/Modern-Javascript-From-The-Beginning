9e2473d34d2cae429f2796f97b097a41ae74f694
Phonekham <30440409+Phonekham@users.noreply.github.com>
2019-01-27 14:19:55 +0700

    ES6 set

--- ESE5/app.js
+++ ESE5/app.js
@@ -101,12 +101,60 @@ console.log(map1.size);
 
 //Iterate maps
 
-// loop using for... of to get keys of values
-for (let [key, value] of map1) {
-  console.log(`${key} = ${value}`);
-} 
+// // loop using for... of to get keys of values
+// for (let [key, value] of map1) {
+//   console.log(`${key} = ${value}`);
+// } 
 
-// Iterate key only
-for (let value of map1.values()) {
-  console.log(value);
-}
+// // Iterate key only
+// for (let value of map1.values()) {
+//   console.log(value);
+// }
+
+
+
+//////////////// Sets - store unique values of any type
+
+const set1 = new Set();
+
+// Add values to set
+set1.add(100);
+set1.add('A string');
+set1.add({name:"phone"});
+set1.add(true);
+set1.add(100);
+console.log(set1);
+
+// const set2 = new Set([1, true, 'string']);
+// console.log(set2);
+
+// console.log(set1);
+
+// Get count
+// console.log(set1.size);
+
+// Check for values
+// console.log(set1.has(100));
+// console.log(set1.has(50 + 50));
+// console.log(set1.has({name: 'John'}));
+
+// Delete from set
+// set1.delete(100);
+
+// console.log(set1);
+
+// ITERATING THROUGH SETS
+
+// For..of 
+// for(let item of set1) {
+//   console.log(item);
+// }
+
+// ForEach Loop
+// set1.forEach((value) => {
+//   console.log(value);
+// });
+
+// CONVERT SET TO ARRAY
+const setArr = Array.from(set1);
+console.log(setArr);
