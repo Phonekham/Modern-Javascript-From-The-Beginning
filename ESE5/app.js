1d625c8be1e38cc119fc2680370bc55c65baad24
Phonekham <30440409+Phonekham@users.noreply.github.com>
2019-01-27 13:56:06 +0700

    Destructuring

--- ESE5/app.js
+++ ESE5/app.js
@@ -40,3 +40,41 @@ const gen = createIds();
 console.log(gen.next().value);
 console.log(gen.next().value);
 console.log(gen.next().value);
+
+// Destructuring assignment
+// let a,b;
+// [a,b] = [100,200];
+
+// // rest pattern
+// [a,b,c,...rest] = [100,200,300,400,500];
+// ({a,b} = {a:100,b:200,c:300,d:400,e:500})
+// console.log(a,b,c);
+
+// Object Destructuring
+
+const person = {
+  name: 'John Doe',
+  age: 32,
+  city: 'Miami',
+  gender: 'Male',
+  sayHello: function () {
+    console.log('Hello');
+  }
+}
+
+// Old ES5
+// const name = person.name,
+//       age = person.age,
+//       city = person.city;
+
+// New ES6 Destructuring
+const {
+  name,
+  age,
+  city,
+  sayHello
+} = person;
+
+console.log(name, age, city);
+
+sayHello();
