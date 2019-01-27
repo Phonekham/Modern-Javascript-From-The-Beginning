7a73e559336a45172ab03a2b78f95f8981e62272
Phonekham <30440409+Phonekham@users.noreply.github.com>
2019-01-27 14:09:51 +0700

    ES6 map

--- ESE5/app.js
+++ ESE5/app.js
@@ -78,3 +78,35 @@ const {
 console.log(name, age, city);
 
 sayHello();
+
+//Map = key-value pairs - can user any type as a key or value
+
+const map1 = new Map();
+
+// set keys
+const key1 = 'some string',
+      key2 = {},
+      key3 = function(){};
+
+//set map values by key
+map1.set(key1, 'Value of key 1');
+map1.set(key2, 'Value of key2');
+map1.set(key3, 'Value of key3');
+
+// Get values by key
+console.log(map1.get(key1), map1.get(key2), map1.get(key3));
+
+//count values
+console.log(map1.size);
+
+//Iterate maps
+
+// loop using for... of to get keys of values
+for (let [key, value] of map1) {
+  console.log(`${key} = ${value}`);
+} 
+
+// Iterate key only
+for (let value of map1.values()) {
+  console.log(value);
+}
